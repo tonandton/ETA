787ca6160b4e3c177dc71ebc1228ddf0ae1e7f4a
tonandton <www.sorasakok@gmail.com>
2025-03-24 22:57:53 +0700

    add transaction

---
+++ backend/controllers/transactionController.js
@@ -0,0 +1,4 @@
+export const addTransaction = async (req, res) => {};
+export const getDashboardInformation = async (req, res) => {};
+export const getTransactions = async (req, res) => {};
+export const transferMoneyToAccount = async (req, res) => {};
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import authRoutes from "./authRoutes.js";
 import accountRoutes from "./accountRoutes.js";
-// import transactionRoutes from "./transactionRoutes";
+import transactionRoutes from "./transactionRoutes.js";
 import userRoutes from "./userRoutes.js";
 
 const router = express.Router();
@@ -9,6 +9,6 @@ const router = express.Router();
 router.use("/auth", authRoutes);
 router.use("/user", userRoutes);
 router.use("/account", accountRoutes);
-// router.use("/transaction", transactionRoutes);
+router.use("/transaction", transactionRoutes);
 
 export default router;
