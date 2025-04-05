08a521a4b43037a64676d82ec654b8be67b02f9d
tonandton <www.sorasakok@gmail.com>
2025-04-05 15:59:34 +0700

    setup file page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,49 @@
 import { useState } from "react";
-import "./App.css";
+import { Navigate, Outlet, Route, Routes } from "react-router-dom";
+import SignIn from "./pages/auth/SignIn";
+import SignUp from "./pages/auth/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Settings from "./pages/Settings";
+import AccountPage from "./pages/AccountPage";
+import Transactions from "./pages/Transactions";
+import useStore from "./store";
+// import "./App.css";
+
+const RootLayout = () => {
+  const user = useStore((state) => state);
+  console.log(user);
+
+  return !user ? (
+    <Navigate to="sign-in" replace={true} />
+  ) : (
+    <>
+      <div>
+        <Outlet></Outlet>
+      </div>
+    </>
+  );
+};
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <h1 className="font-blod text-5xl text-green-400 ">Hello</h1>;
+  return (
+    <main>
+      <div>
+        <Routes>
+          <Route element={<RootLayout />}>
+            <Route path="/" element={<Navigate to="/overview" />} />
+            <Route path="/overview" element={<Dashboard />} />
+            <Route path="/transactions" element={<Transactions />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/account" element={<AccountPage />} />
+          </Route>
+          <Route path="/sign-in" element={<SignUp />} />
+          <Route path="/sign-up" element={<SignIn />} />
+        </Routes>
+      </div>
+    </main>
+  );
 }
 
 export default App;
